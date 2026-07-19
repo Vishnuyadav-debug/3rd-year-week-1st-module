476be0be94aed638c44fa7ec9c6d0690748e4fd5
Vishnuyadav-debug <v5151891@gmail.com>
2026-07-20 01:53:25 +0530

    Add files via upload

---
+++ InverTringle.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"enter a number";
+    cin >> n;
+
+    for (int i = 0; i <n; i++)
+    {
+        for (int j = 0; j <(i+1); j++)
+        {
+            cout<<" ";
+          }
+        for (int k = 0; k < (n-i); k++)
+        {
+            cout<<"*";
+        }
+        cout <<endl;
+        
+        
+    }
+    
+
+
+
+
+    return 0;
+}
